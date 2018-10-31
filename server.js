4178db6b5b2297c58bf7d8b95872382a4b6fc9e8
Lucas Sorensen <lucas.alex.sorensen@gmail.com>
2018-10-31 12:02:36 +0100

    possibly fix port issues?

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 let API_URL
 if (PORT === 80) {
   API_URL = `${HOSTNAME}`
